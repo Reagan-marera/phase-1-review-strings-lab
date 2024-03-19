80aa84431b0bcf4c809ad84f93833d7d66124cee
reagan <marierareagan@gmail.com>
2024-03-19 23:54:32 +0300

    done

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 // Write your code in this file!
+let currentUser = 'Grace Hopper';
+const WelcomeMessage = `welcome to flatbook, ${currentUser}!`
+
+currentUser = 'Grace Hopper';
+const welcomeMessage = `welcome to flatbook, ${currentUser}!`
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+const shortGreeting =`welcome,${currentUser.slice(0,1)}!`;
